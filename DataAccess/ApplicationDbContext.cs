307c14ea5d669249edf0454852edbf869fa7c0fc
Ivaylo Filipov <ivaylofilipov.72@gmail.com>
2022-11-18 14:26:04 +0200

    Added projrct refererences

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -1,7 +1,30 @@
-﻿namespace DataAccess
-{
-    public class ApplicationDbContext
-    {
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
+namespace DataAccess
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext()
+        {
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DB Sets
+
+
+
+
+        // Many-to-many relations, composite key with fluent API
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+        }
     }
 }
