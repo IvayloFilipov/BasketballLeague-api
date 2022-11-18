6cebf2382f9e126e7510542ff77574f7438c5952
Ivaylo Filipov <ivaylofilipov.72@gmail.com>
2022-11-18 13:46:49 +0200

    Sructure initial create

---
+++ BasketballLeague.WEB/Controllers/MatchesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
+
+namespace BasketballLeague.WEB.Controllers
+{
+    [ApiController]
+    [Route("match-results/[conroller]")]
+    public class MatchesController : ControllerBase
+    {
+        public MatchesController()
+        {
+
+        }
+    }
+}
---
+++ BasketballLeague.WEB/Controllers/TeamsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasketballLeague.WEB.Controllers
+{
+    [ApiController]
+    [Route("teams/[controller]")]
+    public class TeamsController : ControllerBase
+    {
+        public TeamsController() 
+        { 
+
+        }
+    }
+}
