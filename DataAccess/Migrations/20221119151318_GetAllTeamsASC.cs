de074feb3361edc1eeac0fd20ce24dbbca4efd26
Ivaylo Filipov <ivaylofilipov.72@gmail.com>
2022-11-19 17:17:04 +0200

    Added two more stored procedures into Migrations

---
+++ DataAccess/Migrations/20221119151318_GetAllTeamsASC.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class GetAllTeamsASC : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"
+CREATE PROC GetAllTeamsASC
+AS
+SELECT * FROM Teams AS t
+ORDER BY t.[Name] ASC
+";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ DataAccess/Migrations/20221119151439_GetAllTeamsDESC.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.Migrations
+{
+    public partial class GetAllTeamsDESC : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sp = @"
+CREATE PROC GetAllTeamsDESC
+AS
+SELECT * FROM Teams AS t
+ORDER BY t.[Name] DESC
+";
+
+            migrationBuilder.Sql(sp);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
